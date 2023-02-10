8cb5f2d69c161fc26338d289149754454c743e13
Esteban Garcia <estebangarcia2121@gmail.com>
2023-02-10 14:10:14 -0800

    Add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
-<script>
+<script lang="ts">
 	import './styles.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="app">
